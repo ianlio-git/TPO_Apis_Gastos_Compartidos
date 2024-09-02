35934db21b4bc9a1cd108774972ad02c520bf8de
Ian Lionetti <ilionetti@uade.edu.ar>
2024-09-01 22:35:13 -0300

    app de prueba

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,18 @@
-import "./App.css";
-
 function App() {
+  const handleClick = () => {
+    window.open("https://www.google.com", "_blank");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className="prueba">Hello World!</p>
+    <div className="App text-center bg-black text-white min-h-screen flex flex-col items-center justify-center">
+      <header className="App-header flex flex-col items-center">
+        <p className="prueba text-red-500 text-xl">Hello World!</p>
+        <button
+          onClick={handleClick}
+          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mt-4"
+        >
+          Click Me
+        </button>
       </header>
     </div>
   );
